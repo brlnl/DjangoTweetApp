091d70933d36ca2c409ea5a39815f3671b5350ec
brlnl <unalberil5@gmail.com>
2025-08-25 12:18:37 +0200

    Clean single-project structure

---
+++ djangotweet/DjangoTweetApp/tweetapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Tweet(models.Model):
+    nickname = models.CharField(max_length=50)
+    message = models.CharField(max_length=280)
+
+    def __str__(self):
+        return f"Tweet nick:{self.nickname} message: {self.message}"
---
+++ djangotweet/DjangoTweetApp/tweetapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+app_name = 'tweetapp'
+
+urlpatterns = [
+    path('', views.listtweet, name='listtweet'),
+    path('addtweet/', views.addtweet, name='addtweet')
+]
